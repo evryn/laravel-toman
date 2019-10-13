00ea16a937e88bfe3b553db8e7bc05bbd277ee84
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2019-10-14 01:34:25 +0330

    Add publisher tests

--- tests/LaravelTomanServiceProviderTest.php
+++ tests/LaravelTomanServiceProviderTest.php
@@ -9,13 +9,42 @@ final class LaravelTomanServiceProviderTest extends TestCase
     /** @test */
     public function publishes_config_correctly()
     {
+        $source = __DIR__ . '/../config/toman.php';
+        $dest = config_path('toman.php');
+
+        $this->assertTrue(unlink($dest));
+
         $this->artisan('vendor:publish', [
-            '--provider' => 'Evryn\LaravelToman\LaravelTomanServiceProvider'
+            '--provider' => 'Evryn\LaravelToman\LaravelTomanServiceProvider',
+            '--tag' => 'config'
         ]);
 
-        $this->assertFileExists(config_path('larapoke.php'));
-        $this->assertFileIsReadable(config_path('larapoke.php'));
-        $this->assertFileEquals(config_path('larapoke.php'), __DIR__ . '/../../config/larapoke.php');
-        $this->assertTrue(unlink(config_path('larapoke.php')));
+        $this->assertFileExists($dest);
+        $this->assertFileIsReadable($dest);
+        $this->assertFileEquals($dest, $source);
+    }
+
+    /** @test */
+    public function publishes_translations_correctly()
+    {
+        $map = [
+            __DIR__ . '/../resources/lang/en/zarinpal.php' => resource_path('lang/vendor/toman/en/zarinpal.php'),
+            __DIR__ . '/../resources/lang/fa/zarinpal.php' => resource_path('lang/vendor/toman/fa/zarinpal.php'),
+        ];
+
+        foreach (array_values($map) as $dest) {
+            $this->assertTrue(unlink($dest));
+        }
+
+        $this->artisan('vendor:publish', [
+            '--provider' => 'Evryn\LaravelToman\LaravelTomanServiceProvider',
+            '--tag' => 'lang'
+        ]);
+
+        foreach ($map as $source => $dest) {
+            $this->assertFileExists($dest);
+            $this->assertFileIsReadable($dest);
+            $this->assertFileEquals($dest, $source);
+        }
     }
 }

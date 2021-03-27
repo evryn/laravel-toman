59b7a5507644472fc6c943c9ece0ecc24908ae4b
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2021-03-28 03:51:14 +0430

    Test requester facade resolves configured Zarinpal gateway

--- tests/Facades/PaymentRequestTest.php
+++ tests/Facades/PaymentRequestTest.php
@@ -3,15 +3,29 @@
 namespace Evryn\LaravelToman\Tests\Facades;
 
 use Evryn\LaravelToman\Facades\PaymentRequest;
+use Evryn\LaravelToman\Gateways\Zarinpal\Requester as ZarinpalRequester;
 use Evryn\LaravelToman\Managers\PaymentRequestManager;
 use Evryn\LaravelToman\Tests\TestCase;
 
 final class PaymentRequestTest extends TestCase
 {
     /** @test */
-    public function resolves_to_gateway_manager()
+    public function resolves_to_configured_zarinpal_gateway()
     {
-        $root = PaymentRequest::getFacadeRoot();
-        self::assertInstanceOf(PaymentRequestManager::class, $root);
+        config([
+            'toman.default' => 'zarinpal',
+            'toman.gateways.zarinpal' => [
+                'sandbox' => true,
+                'merchant_id' => 'xxxxxxxx-yyyy-zzzz-wwww-xxxxxxxxxxxx',
+            ]
+        ]);
+
+        $gateway = PaymentRequest::getFacadeRoot()->driver();
+
+        self::assertInstanceOf(ZarinpalRequester::class, $gateway);
+        self::assertEquals([
+            'sandbox' => true,
+            'merchant_id' => 'xxxxxxxx-yyyy-zzzz-wwww-xxxxxxxxxxxx',
+        ], $gateway->getConfig());
     }
 }

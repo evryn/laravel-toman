4ae072cc87cff9bb3cf4cf9246cba731006d555a
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2021-04-10 05:43:35 +0430

    Remove redundant code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,42 +2,12 @@
 
 namespace Evryn\LaravelToman\Tests;
 
-use Illuminate\Support\Facades\Http;
-use PHPUnit\Framework\Assert as PHPUnit;
+use Evryn\LaravelToman\LaravelTomanServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
-        return ['Evryn\LaravelToman\LaravelTomanServiceProvider'];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Payment' => 'Evryn\LaravelToman\Facades\PaymentRequest',
-        ];
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        self::setupHttpMacros();
-    }
-
-    public static function setupHttpMacros()
-    {
-        Http::macro('assertNthRequestFieldEquals', function ($expected, $field, $nth) {
-            PHPUnit::assertNotEmpty($this->recorded[$nth - 1], "{$nth}th request has not been sent.");
-
-            $request = $this->recorded[$nth - 1][0];
-
-            PHPUnit::assertEquals(
-                $expected,
-                $request->data()[$field],
-                "{$nth}th request's field [{$field}] doesn't equal to [$expected]."
-            );
-        });
+        return [LaravelTomanServiceProvider::class];
     }
 }
